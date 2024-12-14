fdd08f2c3d0131a6d3aeb8031fb224963ec13583
Josim <22josimuddin06@gmail.com>
2024-12-14 23:11:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let navbar = document.getElementById("navbar")
+navbar.style.maxHeight = "0px";
+
+function togglemenu(){
+  if (navbar.stylemaxHeight = "0px")
+  {
+    navbar.style.maxHeight = "300px";
+  }
+}
